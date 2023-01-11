729caead407a6b06970c7db316820f1f9f9b2c7e
imrannn24 <imran200424@icloud.com>
2023-01-11 14:15:08 +0600

    dz

--- 3.task1/main.swift
+++ 3.task1/main.swift
@@ -6,6 +6,22 @@
 //
 
 import Foundation
+print("Введите текст:")
+let a = readLine()!
 
-print("Hello, World!")
+var probely = 0
+
+var simvoly = 0
+
+for i in a{
+    if i == " "{
+        probely += 1
+    }else{
+        simvoly += 1
+    }
+}
+var vsego = probely + simvoly
+print("Количество символов - \(simvoly)")
+print("Количество пробелов - \(probely)")
+print("Всего - \(vsego)")
 
